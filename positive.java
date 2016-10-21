944ee3e6373bae82ae79dfdbb79c62906817c5db
Reshmi-96cse <reshmipugalzhenthi@gmail.com>
2016-10-21 12:52:56 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,11 @@
+class pos
+{
+public static void main(string[] args)
+int a;
+if(a>0)
+system.out.println("positive");
+if(a<0)
+system.out.println("negative");
+else
+sysytem.out.println("equal");
+}
