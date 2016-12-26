03e854d8fc5c2c5e72641c5b8d6281869d3b2342
Reshmi-96cse <reshmipugalzhenthi@gmail.com>
2016-12-26 11:09:22 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+    printf("\nenter the number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("\n**hello**");
+    }
+    getch();
+}
