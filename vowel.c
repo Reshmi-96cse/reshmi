c2010760b7200b3f6c54494b9fc759a38ec7f354
Reshmi-96cse <reshmipugalzhenthi@gmail.com>
2016-12-26 10:21:24 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char b;
+    printf("\n enter the letter");
+    scanf("%c",&b);
+    if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='I'||b=='O'||b=='U')
+    {
+        printf("\n vowel");
+    }
+    else
+    {
+        printf("\nconsanant");
+    }
+    getch();
+}
