74a2a88c2f61b4325f264b68b875100d6aa15568
Reshmi-96cse <reshmipugalzhenthi@gmail.com>
2016-12-26 12:17:38 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,sum=0;
+    printf("\n enter the numbers");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+
+sum+=i;
+
+    }
+       printf("\nsum=%d",sum);
+
+    getch();
+}
