fe97c543ffe22393f7ba0944530cd49075e67b15
Reshmi-96cse <reshmipugalzhenthi@gmail.com>
2016-12-26 10:04:46 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("\n enter the number");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("\neven");
+
+    }
+    else
+    {
+        printf("\n odd");
+    }
+    getch();
+}
